57487be3e8470ed1b2c7be0db013d857959841ce
Adrian Olij <adrix.43@gmail.com>
2021-08-20 19:20:41 -0300

    Initial commit

---
+++ src/main/java/com/ninjabear/apoloweather/ApoloWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.ninjabear.apoloweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApoloWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApoloWeatherApplication.class, args);
+	}
+
+}
